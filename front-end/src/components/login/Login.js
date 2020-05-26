e1bd6eb689cb68532a43d72c3dc2e97576ec3adb
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-26 13:10:27 -0400

    Validation added to Login and Register forms

--- front-end/src/components/login/Login.js
+++ front-end/src/components/login/Login.js
@@ -1,49 +1,126 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
+import * as yup from 'yup';
 
 import './login.css'
 
-//State
-
 let Login = props => {
+
+
+    //State
 
     let [formState, setFormState] = useState({
 
         email: '',
         password: ''
     
-      });
+    });
 
-      //Functions
+    const [inputErrors, setInputErrors] = useState({ //State for form input validation errors
 
-      let formChange = event => {
+        email: "",
+        password: ""
 
-        setFormState({
+    });
 
-            ...formState,
-            [event.target.id]: event.target.value
+    const formSchema = yup.object().shape({
+
+        email: yup
+            .string()
+            .email("- Must be a valid email address.")
+            .required("- Must include email address."),
+        password: yup
+            .string()
+            .min(6, "- Passwords must be at least 6 characters long.")
+            .required(" -Password is Required"),
+    });
+
+
+    //Functions
+
+    let formChange = event => {
+
+    setFormState({
+
+        ...formState,
+        [event.target.id]: event.target.value
 
         })
 
-        console.log(formState);
+        //Sets inputErrors state if invalid input
 
-      }
+        yup
+            .reach(formSchema, event.target.id)
+            .validate(event.target.value)
+            .then(valid => {
 
-      let submitForm = event => {
+                setInputErrors({
 
-        event.preventDefault()
+                    ...inputErrors,
+                    [event.target.id]: ''
 
-      }
+                });
+
+            })
+            .catch(err => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.id]: err.errors[0]
+
+                });
+
+            })
+
+    console.log(formState);
+
+    event.persist()
+
+    }
+
+    let submitForm = event => {
+
+    event.preventDefault()
+
+    }
+
+    useEffect(() => {
+
+    //Input validation schema using Yup
+
+    const formSchema = yup.object().shape({
+
+        email: yup
+            .string()
+            .email("- Must be a valid email address.")
+            .required("- Must include email address."),
+        password: yup
+            .string()
+            .min(6, "- Passwords must be at least 6 characters long.")
+            .required(" -Password is Required"),
+      });
+
+      //Enables button if the input is valid
+
+      formSchema.isValid(formState).then(valid => { 
+
+        document.querySelector('form button').disabled = !valid;
+
+    })
+
+
+    },[formState])
 
     return (
 
-        <div id='login-form'>
+        <div id='form-container'>
 
             <h2>Member Login</h2>
 
             <form onSubmit={event => submitForm(event)}>
 
-                <label htmlFor='email'>Email</label>
+                <label htmlFor='email'>Email {inputErrors.email}</label>
 
                 <input 
                 type='text' 
@@ -52,7 +129,7 @@ let Login = props => {
                 value={formState.email}/>
                 
 
-                <label htmlFor='password'>Password</label>
+                <label htmlFor='password'>Password {inputErrors.password}</label>
 
                 <input 
                 type='text' 
@@ -60,7 +137,7 @@ let Login = props => {
                 onChange={event => formChange(event)}
                 value={formState.password}/>
                     
-                <button>Login</button>
+                <button disabled>Login</button>
 
             </form>
 
--- front-end/src/components/register/Register.js
+++ front-end/src/components/register/Register.js
@@ -1,49 +1,126 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
+import * as yup from 'yup';
 
-import './register.css';
-
-//State
+import './register.css'
 
 let Register = props => {
+
+
+    //State
 
     let [formState, setFormState] = useState({
 
         email: '',
         password: ''
     
-      });
+    });
 
-      //Functions
+    const [inputErrors, setInputErrors] = useState({ //State for form input validation errors
 
-      let formChange = event => {
+        email: "",
+        password: ""
 
-        setFormState({
+    });
 
-            ...formState,
-            [event.target.id]: event.target.value
+    const formSchema = yup.object().shape({
+
+        email: yup
+            .string()
+            .email("- Must be a valid email address.")
+            .required("- Must include email address."),
+        password: yup
+            .string()
+            .min(6, "- Passwords must be at least 6 characters long.")
+            .required(" -Password is Required"),
+    });
+
+
+    //Functions
+
+    let formChange = event => {
+
+    setFormState({
+
+        ...formState,
+        [event.target.id]: event.target.value
 
         })
 
-        console.log(formState);
+        //Sets inputErrors state if invalid input
 
-      }
+        yup
+            .reach(formSchema, event.target.id)
+            .validate(event.target.value)
+            .then(valid => {
 
-      let submitForm = event => {
+                setInputErrors({
 
-        event.preventDefault()
+                    ...inputErrors,
+                    [event.target.id]: ''
 
-      }
+                });
+
+            })
+            .catch(err => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.id]: err.errors[0]
+
+                });
+
+            })
+
+    console.log(formState);
+
+    event.persist()
+
+    }
+
+    let submitForm = event => {
+
+    event.preventDefault()
+
+    }
+
+    useEffect(() => {
+
+    //Input validation schema using Yup
+
+    const formSchema = yup.object().shape({
+
+        email: yup
+            .string()
+            .email("- Must be a valid email address.")
+            .required("- Must include email address."),
+        password: yup
+            .string()
+            .min(6, "- Passwords must be at least 6 characters long.")
+            .required(" -Password is Required"),
+      });
+
+      //Enables button if the input is valid
+
+      formSchema.isValid(formState).then(valid => { 
+
+        document.querySelector('form button').disabled = !valid;
+
+    })
+
+
+    },[formState])
 
     return (
 
-        <div id='register-form'>
+        <div id='form-container'>
 
-            <h2>Register an account</h2>
+            <h2>Register</h2>
 
             <form onSubmit={event => submitForm(event)}>
 
-                <label htmlFor='email'>Email</label>
+                <label htmlFor='email'>Email {inputErrors.email}</label>
 
                 <input 
                 type='text' 
@@ -52,7 +129,7 @@ let Register = props => {
                 value={formState.email}/>
                 
 
-                <label htmlFor='password'>Password</label>
+                <label htmlFor='password'>Password {inputErrors.password}</label>
 
                 <input 
                 type='text' 
@@ -60,9 +137,11 @@ let Register = props => {
                 onChange={event => formChange(event)}
                 value={formState.password}/>
                     
-                <button>Login</button>
+                <button disabled>Submit</button>
 
             </form>
+
+            <Link to='/login'>Already have an account?</Link>
 
         </div>
 
