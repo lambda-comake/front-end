cbe5b957a8c6fa09d7932d5b7eeb318132bf07d9
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-26 17:34:14 -0400

    merged daniels branch

--- front-end/src/components/register/Register.js
+++ front-end/src/components/register/Register.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { Link } from 'react-router-dom';
 import { useHistory, withRouter } from "react-router-dom";
 import {axiosWithAuth} from '../../utils/axiosWithAuth'
@@ -6,40 +7,92 @@ import { connect } from "react-redux";
 import {registerAction} from '../../actions/registerAction'
 import axios from "axios"
 
-import './register.css';
 
-//State
+import * as yup from 'yup';
+
+import './register.css';
 
 let Register = props => {
   const { push } = useHistory();
 
+    //State
 
-  let [formState, setFormState] = useState({
+    let [formState, setFormState] = useState({
 
-    username: '',
-    password: ''
+        username: '',
+        password: ''
+    
+    });
 
-  });
+    const [inputErrors, setInputErrors] = useState({ //State for form input validation errors
 
-  //Functions
+        username: "",
+        password: ""
 
-  let formChange = event => {
+    });
+
+    const formSchema = yup.object().shape({
+
+        username: yup
+            .string()
+            .required("- Must include a username."),
+        password: yup
+            .string()
+            .min(6, "- Passwords must be at least 6 characters long.")
+            .required("- Password is Required"),
+    });
+
+
+    //Functions
+
+    let formChange = event => {
 
     setFormState({
 
-      ...formState,
-      [event.target.id]: event.target.value
+        ...formState,
+        [event.target.id]: event.target.value
 
-    })
+    });
 
     console.log(formState);
 
-  }
+        //Sets inputErrors state if invalid input
 
-  let submitForm = event => {
+        yup
+            .reach(formSchema, event.target.id)
+            .validate(event.target.value)
+            .then(valid => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.id]: ''
+
+                });
+
+            })
+            .catch(err => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.id]: err.errors[0]
+
+                });
+
+            })
+
+    console.log(formState);
+
+    event.persist()
+
+    }
+
+    let submitForm = event => {
 
     event.preventDefault()
-    axiosWithAuth()
+
+        axiosWithAuth()
       .post("auth/register", formState)
       .then((res) => {
         localStorage.setItem("token", (res.data.token));
@@ -54,41 +107,69 @@ let Register = props => {
         alert("There was an error. Please try again.")
         
       });
-  };
 
+
+    }
+
+    useEffect(() => {
+
+    //Input validation schema using Yup
+
+    const formSchema = yup.object().shape({
+
+        username: yup
+            .string()
+            .required("- Must include a username."),
+        password: yup
+            .string()
+            .min(6, "- Passwords must be at least 6 characters long.")
+            .required("- Password is Required"),
+      });
+
+      //Enables button if the input is valid
+
+      formSchema.isValid(formState).then(valid => { 
+
+        document.querySelector('form button').disabled = !valid;
+
+    })
+
+
+    },[formState])
 
 
 return (
 
-  <div id='register-form'>
+        <div id='form-container'>
 
-    <h2>Register an account</h2>
+            <h2>Register</h2>
 
-    <form onSubmit={event => submitForm(event)}>
+            <form onSubmit={submitForm}>
 
-      <label htmlFor='email'>Email</label>
+                <label htmlFor='username'>Username {inputErrors.username}</label>
 
-      <input
-        type='text'
-        name='username'
-        id='username'
-        onChange={event => formChange(event)}
-        value={formState.email} />
+                <input 
+                type='text' 
+                id='username' 
+                onChange={event => formChange(event)}
+                value={formState.username}/>
+                
 
+                <label htmlFor='password'>Password {inputErrors.password}</label>
 
-      <label htmlFor='password'>Password</label>
+                <input 
+                type='password' 
+                id='password' 
+                onChange={event => formChange(event)}
+                value={formState.password}/>
+                    
+                <button disabled>Submit</button>
 
-      <input
-        type='text'
-        id='password'
-        onChange={event => formChange(event)}
-        value={formState.password} />
+            </form>
 
-      <button>Login</button>
+            <Link to='/login'>Already have an account?</Link>
 
-    </form>
-
-  </div>
+        </div>
 
 )
 
