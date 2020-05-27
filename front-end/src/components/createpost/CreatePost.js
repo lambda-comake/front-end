f82241f95222dd463e10c79d9231f0e342716a1a
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-27 14:51:34 -0400

    Added state for input inside CreatePost

--- front-end/src/components/createpost/CreatePost.js
+++ front-end/src/components/createpost/CreatePost.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './createpost.css'
 
 let CreatePost = props => {
 
+    //State
+
+    let [formState, setFormState] = useState({
+
+        title: '',
+        description: ''
+    
+    });
+
+    //Functions
+
+    let formChange = event => {
+
+        setFormState({
+    
+            ...formState,
+            [event.target.id]: event.target.value
+    
+        })
+
+    }
+
+    let submitForm = event => {
+
+        event.preventDefault();
+
+    }
+
     return (
 
-        <form className='createpost'>
+        <form className='createpost' onSubmit={event => submitForm(event)}>
 
             <label htmlFor='title'>Title</label>
-            <input id='title' placeholder='Type your title here'></input> {/* How to stop pressing 'enter' inside the title input from submitting the form? */}
+
+            <input 
+            id='title' 
+            placeholder='Type your title here' 
+            onChange={event => formChange(event)}
+            value={formState.title}/> {/* How to stop pressing 'enter' inside the title input from submitting the form? */}
 
             <label htmlFor='description'>Issue Description</label>
-            <textarea id='description' placeholder='Tell us about the issue'></textarea>
+
+            <textarea 
+            id='description' 
+            placeholder='Tell us about the issue'
+            onChange={event => formChange(event)}
+            value={formState.description}></textarea>
 
             <button>Submit</button>
 
