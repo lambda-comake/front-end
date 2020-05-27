17efde8121663ccfecc023c618e99942c96119d7
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-27 15:21:37 -0400

    AddIssue has state and input validation

--- front-end/src/components/addissue/AddIssue.js
+++ front-end/src/components/addissue/AddIssue.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import * as yup from 'yup';
 
 import './addissue.css'
 
@@ -13,6 +15,25 @@ let AddIssue = props => {
     
     });
 
+    const [inputErrors, setInputErrors] = useState({ //State for form input validation errors
+
+        title: "",
+        description: ""
+
+    });
+
+    const formSchema = yup.object().shape({
+
+        title: yup
+            .string()
+            .min(5, "Title must be at least 5 characters long.")
+            .required("Must include a title."),
+        description: yup
+            .string()
+            .min(20, "Description must be at least 20 characters long.")
+            .required("Description is Required"),
+    });
+
     //Functions
 
     let formChange = event => {
@@ -24,7 +45,47 @@ let AddIssue = props => {
     
         })
 
+        yup
+            .reach(formSchema, event.target.id)
+            .validate(event.target.value)
+            .then(valid => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.id]: ''
+
+                });
+
+            })
+            .catch(err => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.id]: err.errors[0]
+
+                });
+
+            })
+
+
+        event.persist();
+
     }
+
+    useEffect(_ => {
+
+        //Enabled submit button if input is valid
+
+        formSchema.isValid(formState).then(valid => { 
+
+            document.querySelector('form button').disabled = !valid;
+    
+        })
+
+    },[formState]);
+
 
     let submitForm = event => {
 
@@ -34,9 +95,11 @@ let AddIssue = props => {
 
     return (
 
-        <form className='createpost' onSubmit={event => submitForm(event)}>
+        <form className='addissue' onSubmit={event => submitForm(event)}>
 
             <label htmlFor='title'>Title</label>
+
+            <p>{inputErrors.title}</p>
 
             <input 
             id='title' 
@@ -46,13 +109,15 @@ let AddIssue = props => {
 
             <label htmlFor='description'>Issue Description</label>
 
+            <p>{inputErrors.description}</p>
+
             <textarea 
             id='description' 
             placeholder='Tell us about the issue'
             onChange={event => formChange(event)}
             value={formState.description}></textarea>
 
-            <button>Submit</button>
+            <button disabled>Submit</button>
 
         </form>
 
