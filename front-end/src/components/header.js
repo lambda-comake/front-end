9c001f2b6b9cbc045beedef5b96f6307e0b244bd
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-29 07:54:49 -0400

    Styled header and issueform more

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 
+import './header.css'
+
 const Header=() => {
     return (
-<>
-<p>header!!!!!</p>
-          <Link to="/">Login</Link>
-           <Link to="/register">Sign Up</Link>
-           <Link href="https://lambda-comake.github.io/UI/">More Info</Link>
-</>
+        <nav id='topNav'>
+
+            <Link to="/">Login</Link>
+            <Link to="/register">Sign Up</Link>
+            <Link href="https://lambda-comake.github.io/UI/">More Info</Link>
+
+        </nav>
     )
 }
 export default Header
